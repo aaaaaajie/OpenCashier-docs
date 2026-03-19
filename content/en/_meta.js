25efbb7047ee100b05b5e4063e29c16b0ec4439a
aaaaaajie <m18210870360@163.com>
2026-03-19 17:15:59 +0800

    docs: add contributing guidelines in English and Chinese, update meta files

--- content/en/_meta.js
+++ content/en/_meta.js
@@ -2,5 +2,6 @@ export default {
   index: 'Home',
   deployment: 'Deployment Guide',
   'merchant-api-integration': 'Merchant Integration',
-  'provider-config-reference': 'Provider Config Reference'
+  'provider-config-reference': 'Provider Config Reference',
+  contributing: 'Contributing'
 }
--- content/zh-CN/_meta.js
+++ content/zh-CN/_meta.js
@@ -2,5 +2,6 @@ export default {
   index: '首页',
   deployment: '部署指南',
   'merchant-api-integration': '商户接入指南',
-  'provider-config-reference': '渠道配置参考'
+  'provider-config-reference': '渠道配置参考',
+  contributing: '参与贡献'
 }
