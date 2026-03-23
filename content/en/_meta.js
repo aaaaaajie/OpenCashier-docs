f51f7c8dd5b8ba563bb56bea8cf15a5229374394
aaaaaajie <m18210870360@163.com>
2026-03-23 15:03:00 +0800

    docs: add Merchant Quickstart Guide and update related documentation in English and Chinese

--- content/en/_meta.js
+++ content/en/_meta.js
@@ -2,6 +2,7 @@ export default {
   index: 'Home',
   deployment: 'Deployment Guide',
   'merchant-api-integration': 'Merchant Integration',
+  'merchant-quickstart': 'Merchant Quickstart Guide',
   'node-sdk': 'Node SDK Guide',
   'provider-config-reference': 'Provider Config Reference',
   contributing: 'Contributing'
--- content/zh-CN/_meta.js
+++ content/zh-CN/_meta.js
@@ -2,6 +2,7 @@ export default {
   index: '首页',
   deployment: '部署指南',
   'merchant-api-integration': '商户接入指南',
+  'merchant-quickstart': 'Merchant Quickstart 指南',
   'node-sdk': 'Node SDK 接入',
   'provider-config-reference': '渠道配置参考',
   contributing: '参与贡献'
